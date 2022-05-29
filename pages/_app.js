77abaf0ac5b3afdfca303e29ea40bfa857695568
Nwele Uchenna <nweleuchenna2004@gmail.com>
2022-05-29 11:45:51 +0100

    finished with the build

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import "swiper/css/bundle";
+import "swiper/css";
+import "swiper/css/pagination";
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
